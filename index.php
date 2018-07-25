92ad9d0e5dfa14ff128d524899b74601299d3bd9
Nzingani <isabelnzingani@outlook.com>
2018-07-25 21:39:07 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8" />
+    <title>exo5</title>
+  </head>
+  <body>
+  <p>
+    <?php
+    $init = null;
+    echo $init;
+    $init = 0;
+    echo $init;
+    ?>
+  </p>
+
+  </body>
